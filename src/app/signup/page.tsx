abc967a600aa865a05e3147b1ddc9c5120e76f36
TainYanTun <166300382+TainYanTun@users.noreply.github.com>
2025-06-10 23:28:52 +0700

    Another one

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,8 +4,10 @@ import { useRouter } from "next/navigation";
 import { hashPassword } from "../lib/auth";
 
 export default function SignupPage() {
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [ID,setID] = useState("");
+  const [phone, setPhone] =  useState("");
   const router = useRouter();
 
   async function handleSubmit(e) {
@@ -16,7 +18,7 @@ export default function SignupPage() {
     const response = await fetch("/api/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password: hashedPassword }),
+      body: JSON.stringify({ username, ID, phone, password: hashedPassword }),
     });
     const result = await response.json();
     console.log(result);
@@ -28,16 +30,28 @@ export default function SignupPage() {
   return (
     <form onSubmit={handleSubmit}>
       <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
       />
       <input
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
+      />
+      <input
+        type="text"
+        value={ID}
+        onChange ={(e) => setID(e.target.value)}
+        placeholder="National ID or Passport ID"
+      />
+      <input
+        type="text"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        placeholder="Phone Number"
       />
       <button type="submit">Sign Up</button>
     </form>
