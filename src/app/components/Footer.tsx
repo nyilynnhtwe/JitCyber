d841c886061da3a28e0e123e3586c99db28b6db8
TainYanTun <166300382+TainYanTun@users.noreply.github.com>
2025-06-18 23:16:03 +0700

    Co-authored-by: Pann Ei Ko Ko <p-e-koko@users.noreply.github.com>

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -69,7 +69,7 @@ export default function Footer({ t, locale }: FooterProps) {
               </li>
             </ul>
             <div className="mt-4">
-              <Link href="/contact_form">
+              <Link href="./contact_form">
               <button
                 className="bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition-colors"
               >
